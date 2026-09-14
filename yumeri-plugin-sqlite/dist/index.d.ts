028f7ac670a9f6a170416c26a2f7e601db319f1c
FireGuo1145 <fireguo1145@flweb.cn>
2026-09-14 12:44:22 +0800

    chore: Stop tracking build output

--- yumeri-plugin-sqlite/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Context, Schema } from 'yumeri';
-export declare const provide: string[];
-export interface SqliteConfig {
-    path: string;
-}
-export declare const config: Schema<SqliteConfig>;
-export declare function apply(ctx: Context, config: SqliteConfig): Promise<void>;
-export declare function disable(ctx: Context): Promise<void>;
